5adceb7c111f1bbfe6dfcfe9b3baeb38a66c44da
Umer MiB <mohammadumer704@gmail.com>
2021-01-05 20:31:02 +0500

    created quickstart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
+
+  const callAPI = async () => {
+    await Axios.get(
+      "https://wah4xd25j9.execute-api.eu-central-1.amazonaws.com/prod/api/v1/checkuser?email=owaistest@gmail.com&park_id=2"
+    )
+      .then((res) => setData(JSON.stringify(res.data)))
+      .catch((err) =>
+        setError(
+          JSON.stringify(
+            err?.response?.data?.message
+              ? err?.response?.data?.message
+              : err.message
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+  return <div className="App">{data ? data : error}</div>;
 }
 
 export default App;
